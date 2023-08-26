371e9d85070068da70b71d8152742469bcf8bd08
acoyp99 <acoyp1999@gmail.com>
2023-08-26 00:47:14 -0500

    jenkinsfile fix

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -16,7 +16,7 @@ resource "aws_iam_role" "ec2_role" {
 resource "aws_instance" "example" {
   count = 2
 
-  ami           = "ami-0c55b159cbfafe1f0" # Change this to your desired AMI
+  ami           = "ami-06be7c79234a3be48" # Change this to your desired AMI
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.private.id
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
